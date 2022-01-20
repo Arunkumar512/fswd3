d632a551b8c7bb40f64ed16fa7a996c65b37c086
Arunkumar <arunkumar512199@gmail.com>
2022-01-20 19:24:45 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const body = document.querySelector("body");
-const task = ["abcde", "asdfff", "asffgg"];
+const button = document.querySelector('button')
+const input = document.querySelector('input#todolistg')
+const list = document.querySelector('ol')
 
-const ol = document.getElementById("ol");
 
-task.forEach((task) => {
-  const li = document.createElement("li");
-  const testnode = document.createTextNode(task);
-  li.appendChild(testnode);
-  ol.appendChild(li);
-});
-body.appendChild(ol);
+const handleAdd = (e) => {
+
+    const textToAdd = input.value
+
+    if (textToAdd.length > 0) {
+        const ele = document.createElement('li')
+        const text_node = document.createTextNode(textToAdd)
+
+        ele.appendChild(text_node)
+        list.appendChild(ele)
+        // create a dynamic button element
+        const button = document.createElement('button')
+        button.innerHTML = "Delete"
+        ele.appendChild(button)
+
+        button.addEventListener('click', (e) => {
+            ele.remove()
+        })
+
+
+        // clear the list
+        input.value = ''
+    } else {
+        window.alert("Task can not be empty")
+    }
+
+}
+
+button.addEventListener('click', handleAdd)
