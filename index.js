6cc2ef0f1043b1c4601b6de5a49d784b6cb5d51b
Arunkumar <arunkumar512199@gmail.com>
2022-01-20 18:04:22 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function abc(){
+const body = document.querySelector("body");
+const task = ["abcde", "asdfff", "asffgg"];
 
+const ol = document.getElementById("ol");
 
-
-
-    
-}
+task.forEach((task) => {
+  const li = document.createElement("li");
+  const testnode = document.createTextNode(task);
+  li.appendChild(testnode);
+  ol.appendChild(li);
+});
+body.appendChild(ol);
