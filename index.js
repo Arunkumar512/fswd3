672e292681f1f92918a13348abee322d375e5a03
Arunkumar <arunkumar512199@gmail.com>
2022-01-21 19:29:20 +0530

    new

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input#todolistg')
-const list = document.querySelector('ol')
+const body=document.querySelector("body")
+const choice_container=document.querySelector("div.choice")
+const rock=document.querySelector("img#rock")
+const paper=document.querySelector("img#paper")
+const scissor=document.querySelector("img#scissor")
 
-
-const handleAdd = (e) => {
-
-    const textToAdd = input.value
-
-    if (textToAdd.length > 0) {
-        const ele = document.createElement('li')
-        const text_node = document.createTextNode(textToAdd)
-
-        ele.appendChild(text_node)
-        list.appendChild(ele)
-        // create a dynamic button element
-        const button = document.createElement('button')
-        button.innerHTML = "Delete"
-        ele.appendChild(button)
-
-        button.addEventListener('click', (e) => {
-            ele.remove()
-        })
-
-
-        // clear the list
-        input.value = ''
-    } else {
-        window.alert("Task can not be empty")
-    }
-
+const random=()=>{
+    const val = (Math.floor((Math.random().toFixed(1) * 3)))
+const game = (user_choice)=>{
+    
 }
 
-button.addEventListener('click', handleAdd)
+    switch (user_choice){
+case 0:
+  return "rock"
+case 1:
+  return "paper"
+case 2:
+  return "scissor"
+default:
+  return "paper"
+    }
+}
+rock.addEventListener('click', () => {
+    console.log(game('rock'))
+})
+paper.addEventListener('click', (e) => {
+    console.log(game('paper'))
+})
+scissor.addEventListener('click', (e) => {
+    console.log(game('scissor'))
+})
