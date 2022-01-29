52ed2add334041c936f0ae44fc983a9462a2fb94
Arunkumar <arunkumar512199@gmail.com>
2022-01-29 20:03:22 +0530

    recipe changes

--- index.js
+++ index.js
@@ -1,94 +1,13 @@
-// let value = 11;
+const application_id = "ab15bb07";
+const application_key = "a3648852bc281474a6541be616ea4632";
+// const search="pizza"
+//console.log(endpoint)
 
-// const myFirstPromise = new Promise((resolve, reject) => {
-//   if (value > 10) resolve([{ id: 1, data: "this is data" }]);
-//   else reject({ error: "error fetching data" });
-// });
-
-// myFirstPromise.then((response) => {
-//   console.log(response);
-// });
-// myFirstPromise
-//   .then(response => response[0])
-//   .then(data => {
-//     console.log(data)
-//   }).catch(err => { console.log(err) })
-
-// // .catch(error=>{
-//     console.log(error);
-// })
-
-//make an api call to A
-/*
-then use the data from A to make another api call to B 
-then use the data from A to make another api call to B 
-then use the data from a to make another api call to B 
-then use the data from a to make another api call to B 
-then use the data from a to make another api call to B 
-then use the data from a to make another api call to B 
-then use the data from a to make another api call to B 
-
-
-*/
-// const getdata = () =>{
-
-// }
-// fetch('https://www.google.com/')
-// .then(response => response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(response=> response.data)
-// .then(data => {
-//      console.log(data)
-//     }
-//  ).catch(err => {
-//      console.log(err)
-//  })
-// console.log(1);
-// fetch()
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data)
-//   });
-// console.log(2);
-
-// const getdata = async () => {
-//   console.log(1);
-//   const response = await fetch();
-//   const data = await response.json();
-//   console.log(data);
-// };
-// getdata()
-// console.log(3)
-
-const getData = async () => {
-  console.log(1);
-
-  // await fetch('https://jsonplaceholder.typicode.com/todos')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //         console.log(data)
-  //     })
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    console.log("i will execute ");
-  }
+const getRecipe = async (query) => {
+  const endpoint = `https://api.edamam.com/search?q=${query}&app_id=${application_id}&app_key=${application_key}`;
+  console.log(endpoint);
+  const res = await fetch(endpoint);
+  const data = await res.json();
+  console.log(data);
 };
-
-getData();
-
-console.log(3);
+getRecipe("Pizza");
