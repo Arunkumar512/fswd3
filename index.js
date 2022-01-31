aad8409c1df28f55c46b777babca3e0e3167e57f
Arunkumar <arunkumar512199@gmail.com>
2022-01-31 18:57:52 +0530

    revise

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const application_id = "ab15bb07";
-const application_key = "a3648852bc281474a6541be616ea4632";
-// const search="pizza"
-//console.log(endpoint)
+const myFakePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      data: [1, 2, 3, 4, 5],
+      message: "I am fullfilled",
+    });
+  }, 3000);
+});
 
-const getRecipe = async (query) => {
-  const endpoint = `https://api.edamam.com/search?q=${query}&app_id=${application_id}&app_key=${application_key}`;
-  console.log(endpoint);
-  const res = await fetch(endpoint);
-  const data = await res.json();
-  console.log(data);
+const getData = async () => {
+  const res = await myFakePromise;
+  console.log(res);
 };
-getRecipe("Pizza");
+
+try {
+  getData();
+} catch (error) {
+  console.log(error);
+} 
