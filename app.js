b17141d45161e313fcfaf0481e138037936ff593
Arunkumar <arunkumar512199@gmail.com>
2022-02-05 13:02:58 +0530

    add

--- app.js
+++ app.js
@@ -2,14 +2,33 @@
 // ReactDOM.render(
 // ele,
 // document.getElementById('root'))
-// const ele = document.createElement('h1') 
+// const ele = document.createElement('h1')
 // ele.innerText = "hello listen"
-// document.getElementById('root').appendChild(ele) 
+// document.getElementById('root').appendChild(ele)
 // console.log(ele)
 
+// const ele =document.createElement("h1")
+// ele.innerText="asdsffds"
+// document.getElementById("root").appendChild(ele)
+// console.log(ele)
+let counter = 0;
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const count = document.getElementById("count");
+const ele = document.getElementById("ele");
 
+plus.addEventListener("click", (e) => {
+  counter++;
+  count.innerHTML = counter;
+  console.log(counter);
+  ele.style=`height:${counter}px`
+  console.log(ele.style)
+});
 
-const ele =document.createElement("h1")
-ele.innerText="asdsffds"
-document.getElementById("root").appendChild(ele)
-console.log(ele)
+minus.addEventListener("click", (e) => {
+  counter--;
+  count.innerHTML = counter;
+  console.log(counter);
+  ele.style=`height:${counter}px`
+  console.log(ele.style)
+});
